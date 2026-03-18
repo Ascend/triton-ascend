652e2c969abf010dd19dfc9dd504265c9c826b54
wangzhanpeng5 <wangzhanpeng5@huawei.com>
2026-03-18 15:09:12 +0800

    fix(min):Keep tie_break_left=false consistent with upstream Triton.
    Co-authored-by: wangzhanpeng5<wangzhanpeng5@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1404 merge 0318_main_tie_break_left_rebort into main
    
    fix(min):Keep tie_break_left=false consistent with upstream Triton.
    
    Created-by: wangzhanpeng5
    Commit-by: wangzhanpeng5
    Merged-by: ascend-robot
    Description: To align with the semantics of upstream Triton: when tie_break_left=false, it does not mean tie_break_right; 
    rather, it indicates that tie-breaking is not handled at all. 
    The ttadapter will always pass tie_break_left=True, and tie-breaking will be handled by NPUIR.
    
    See merge request: Ascend/triton-ascend!1404

--- python/triton/language/standard.py
+++ python/triton/language/standard.py
@@ -141,8 +141,7 @@ def _argmax_combine(value1, index1, value2, index2, tie_break_left):
     if tie_break_left:
         tie = value1 == value2 and index1 < index2
     else:
-        # matchTieBreakResult need [eq/sgt/and] to match
-        tie = value1 == value2 and index1 > index2
+        tie = False
     gt = value1 > value2 or tie
     v_ret = core.where(gt, value1, value2)
     i_ret = core.where(gt, index1, index2)
@@ -212,8 +211,7 @@ def _argmin_combine(value1, index1, value2, index2, tie_break_left):
     if tie_break_left:
         tie = value1 == value2 and index1 < index2
     else:
-        # matchTieBreakResult need [eq/sgt/and] to match
-        tie = value1 == value2 and index1 > index2
+        tie = False
     lt = value1 < value2 or tie
     value_ret = core.where(lt, value1, value2)
     index_ret = core.where(lt, index1, index2)
