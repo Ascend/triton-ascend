b613cc754e4ae58d2401614ad30d255dc3932293
AmmarDab3an <haj.ahmad.ammar@huawei.com>
2026-03-23 14:07:54 +0800

    feat: a5 inject-block-all
    Co-authored-by: AmmarDab3an<haj.ahmad.ammar@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1435 merge feature-a5-inject-block-all into main
    
    feat: a5 inject-block-all
    
    Created-by: AmmarDab3an
    Commit-by: AmmarDab3an
    Merged-by: ascend-robot
    Description: The core Triton is a small number of people, and we receive many PRs (thank
    you!).  To help us review your code more quickly, **if you are a new
    contributor (less than 3 PRs merged) we ask that you complete the following
    tasks and include the filled-out checklist in your PR description.**
    
    Complete the following tasks before sending your PR, and replace `[ ]` with
    `[x]` to indicate you have done them.
    
    - [ ] I am not making a trivial change, such as fixing a typo in a comment.
    
    - [ ] I have written a PR description following these
      [rules](https://cbea.ms/git-commit/#why-not-how).
    
    - [ ] I have run `pre-commit run --from-ref origin/main --to-ref HEAD`.
    
    - Select one of the following.
      - [ ] I have added tests.
        - `/test` for `lit` tests
        - `/unittest` for C++ tests
        - `/python/test` for end-to-end tests
      - [ ] This PR does not need a test because `FILL THIS IN`.
    
    - Select one of the following.
      - [ ] I have not added any `lit` tests.
      - [ ] The `lit` tests I have added follow these [best practices](https://mlir.llvm.org/getting_started/TestingGuide/#filecheck-best-practices),
        including the "tests should be minimal" section. (Usually running Python code
        and using the instructions it generates is not minimal.)
    
    
    See merge request: Ascend/triton-ascend!1435

--- third_party/ascend/backend/compiler.py
+++ third_party/ascend/backend/compiler.py
@@ -460,6 +460,11 @@ def linalg_to_bin_enable_npu_compile_910_95(linalg: str, metadata, opt):
             _compile_option_list += \
                 [f"--enable-hivm-inject-barrier-all-sync={inject_barrier_all}"]
 
+        inject_block_all = metadata["inject_block_all"]
+        if inject_block_all is not None:
+            _compile_option_list += \
+                [f"--enable-hivm-inject-block-all-sync={inject_block_all}"]
+
         limit_auto_multi_buffer_only_for_local_buffer = metadata["limit_auto_multi_buffer_only_for_local_buffer"]
         if limit_auto_multi_buffer_only_for_local_buffer is not None:
             _compile_option_list += \
