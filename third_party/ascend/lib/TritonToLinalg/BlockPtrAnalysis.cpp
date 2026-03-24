e22b286479d56ef42b0539369f34037aafa56137
wutianyao <wutianyao1@huawei.com>
2026-03-24 20:51:09 +0800

    fix: Move makeTensorPtrOp cache after redundantOp create
    Co-authored-by: wutianyao<wutianyao1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1414 merge main4 into main
    
    fix: Move makeTensorPtrOp cache after redundantOp create
    
    Created-by: wutianyao
    Commit-by: wutianyao
    Merged-by: ascend-robot
    Description: When makeblockptr is rewritten, a redundantOp is generated. The source of blockdata should be directed to this newly generated op and then stored in the cache. Otherwise, when the blockdata is retrieved from the cache and reinterpretcast is created based on the source, the sequence will be incorrect.
    
    ----
    
    The core Triton is a small number of people, and we receive many PRs (thank
    you!).  To help us review your code more quickly, **if you are a new
    contributor (less than 3 PRs merged) we ask that you complete the following
    tasks and include the filled-out checklist in your PR description.**
    
    Complete the following tasks before sending your PR, and replace `[ ]` with
    `[x]` to indicate you have done them.
    
    - [ ] I am not making a trivial change, such as fixing a typo in a comment.
    
    - [ ] I have written a PR description following these
      [rules](https://cbea.ms/git-commit/#why-not-how).
    
    - [ ] I have run `pre-commit run --from-ref origin/main --to-ref HEAD`.
    
    - Select one of the following.
      - [ ] I have added tests.
        - `/test` for `lit` tests
        - `/unittest` for C++ tests
        - `/python/test` for end-to-end tests
      - [ ] This PR does not need a test because `FILL THIS IN`.
    
    - Select one of the following.
      - [ ] I have not added any `lit` tests.
      - [ ] The `lit` tests I have added follow these [best practices](https://mlir.llvm.org/getting_started/TestingGuide/#filecheck-best-practices),
        including the "tests should be minimal" section. (Usually running Python code
        and using the instructions it generates is not minimal.)
    
    
    See merge request: Ascend/triton-ascend!1414

--- third_party/ascend/lib/TritonToLinalg/BlockPtrAnalysis.cpp
+++ third_party/ascend/lib/TritonToLinalg/BlockPtrAnalysis.cpp
@@ -1247,8 +1247,6 @@ void BlockDataParser::rewriteMakeTensorPtrOp(
     assert(data.getRank() == 1);
   }
 
-  known[op.getResult()] = data;
-
   // special handling for davinci
   // create redundant reinterpret_cast op for record shape info
   auto redundantOp = createRedundantOp(op, rewriter, data);
@@ -1256,6 +1254,7 @@ void BlockDataParser::rewriteMakeTensorPtrOp(
 
   // create reinterpret_cast op for the target block
   data.setSource(redundantOp.getResult());
+  known[op.getResult()] = data;
   auto castOp = data.createCastOp(resultShape, loc, rewriter);
   rewriter.replaceOp(op, castOp.getResult());
 
