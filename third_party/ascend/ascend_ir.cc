3faf033d50b213253936b9c8d686f2f6b632ae40
gymgit1 <guoyiming17@huawei.com>
2026-04-07 09:31:55 +0800

    fix : resolve CleanCode warnings
    Co-authored-by: gymgit1<guoyiming17@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1549 merge cleancode into release/3.5.x
    
    fix : resolve CleanCode warnings
    
    Created-by: gymgit1
    Commit-by: gymgit1
    Merged-by: ascend-robot
    Description: ![image.png](https://raw.gitcode.com/user-images/assets/7623225/7d96c032-de54-45cb-9e8f-77e7f79d1d3c/image.png 'image.png')The core Triton is a small number of people, and we receive many PRs (thank
    you!).  To help us review your code more quickly, **if you are a new
    contributor (less than 3 PRs merged) we ask that you complete the following
    tasks and include the filled-out checklist in your PR description.**
    
    Complete the following tasks before sending your PR, and replace `[ ]` with
    `[x]` to indicate you have done them.
    
    - [ ] I am not making a trivial change, such as fixing a typo in a comment.
    
    - [ ] I have written a PR description following these
      [rules](https://cbea.ms/git-commit/#why-not-how).
    
    - [ ] I have run `pre-commit run --from-ref origin/main --to-ref HEAD`.
    
    - Select one of the following.
      - [ ] I have added tests.
        - `/test` for `lit` tests
        - `/unittest` for C++ tests
        - `/python/test` for end-to-end tests
      - [ ] This PR does not need a test because `FILL THIS IN`.
    
    - Select one of the following.
      - [ ] I have not added any `lit` tests.
      - [ ] The `lit` tests I have added follow these [best practices](https://mlir.llvm.org/getting_started/TestingGuide/#filecheck-best-practices),
        including the "tests should be minimal" section. (Usually running Python code
        and using the instructions it generates is not minimal.)
    
    
    See merge request: Ascend/triton-ascend!1549

--- third_party/ascend/ascend_ir.cc
+++ third_party/ascend/ascend_ir.cc
@@ -52,7 +52,7 @@ struct AscendNPUIROpBuilder : public TritonOpBuilder {
   explicit AscendNPUIROpBuilder(MLIRContext *context, std::string target = "")
       : TritonOpBuilder(context), target(target) {}
 
-  bool is_910_95()
+  bool is_910_95() const
   {
     // TODO: Use enum instead of strings after enabling HACC in satandalone
     // build
