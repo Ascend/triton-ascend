cb206b8be3d8d9599247422fad1e57c9db0c63b0
luobaiqing <luobaiqing1@huawei.com>
2026-04-08 10:28:31 +0800

    fix(Simt Template): when mask is discrete and ptr is continuous, we should also change it to indirect_load

--- third_party/ascend/lib/TritonToUnstructure/UnstructureConversionPass.cpp
+++ third_party/ascend/lib/TritonToUnstructure/UnstructureConversionPass.cpp
@@ -240,6 +240,7 @@ LogicalResult UnstructuredMemAccessConverter<MemAccOpTy>::matchAndRewrite(
 
   auto ptr = op.getPtr();
   auto ptrType = dyn_cast<RankedTensorType>(ptr.getType());
+  auto isDiscreteMask = op->hasAttr("is_discrete_mask");
 
   if (auto ptrPtrType = dyn_cast<triton::PointerType>(ptr.getType())) {
     if (auto ptrTensorType =
@@ -257,10 +258,9 @@ LogicalResult UnstructuredMemAccessConverter<MemAccOpTy>::matchAndRewrite(
   if (checkUnstructureAnnotated(op, rewriter))
     ptrOffsetInfo.setUnstructured(ptrOffsetInfo.getRank());
 
-  if (ptrOffsetInfo.isStructured() &&
-      (!ptrOffsetInfo.isScalarLike() ||
-       llvm::all_of(ptrType.getShape(), [](int64_t dim) { return dim == 1; })))
-    return failure();
+  if (ptrOffsetInfo.isStructured() && !isDiscreteMask &&
+      (!ptrOffsetInfo.isScalarLike() || llvm::all_of(ptrType.getShape(), [](int64_t dim) { return dim == 1; })))
+      return failure();
 
   LLVM_DEBUG({
     auto &os = llvm::dbgs();
@@ -350,7 +350,7 @@ LogicalResult UnstructuredMemAccessConverter<MemAccOpTy>::matchAndRewrite(
   });
 
   // Fast path on A5: rewrite tt.load/store to tt.indirect_load/store directly.
-  if (compileOn91095Flag && forceSimtTemplateFlag && ptrOffsetInfo.isUnstructuredOrScalarlike()) {
+  if (compileOn91095Flag && forceSimtTemplateFlag && (ptrOffsetInfo.isUnstructuredOrScalarlike() || isDiscreteMask)) {
     if constexpr (std::is_same_v<MemAccOpTy, triton::LoadOp>) {
       assert(isa<triton::PointerType>(srcPtr.getType()) && "src must be ptr type");
       Value mask = op.getMask();
